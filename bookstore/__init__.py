a720575bb0a023e286a6c9a108db1cc9a7c0bdf0
Kyle Kelley <kyle.kelley@rackspace.com>
2013-08-04 21:00:29 -0500

    Version bump

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -6,8 +6,8 @@ Stores IPython notebooks automagically onto OpenStack clouds through Swift.
 '''
 
 __title__ = 'bookstore'
-__version__ = '0.0.4a'
-__build__ = 0x000004
+__version__ = '0.1.0'
+__build__ = 0x000100
 __author__ = 'Kyle Kelley'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2013 Kyle Kelley'
